e1ca46eeca4211627f87e52283d36b7e659d9366
Christy Chovalloor <christy.chovalloor@gmail.com>
2025-08-20 18:39:51 +0530

    Added version 1.1.0  requisites and changes

--- 04_src/jee_dropout_model.py
+++ 04_src/jee_dropout_model.py
@@ -1,16 +1,11 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-import shap
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.svm import SVC
 import joblib
-import pickle
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline 
-from sklearn.metrics import classification_report
-import joblib
-
 
 jee_final = pd.read_csv("01_Data/03_final/JEE_Dropout_Final.csv", delimiter=',')
 
@@ -21,13 +16,19 @@ Y= jee_final["dropout"]
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
 
 
-pipeline_B = Pipeline([
+pipeline= Pipeline([
   ('smote', SMOTE(random_state=42)),
-  ('rfc', RandomForestClassifier(random_state=42, class_weight='balanced', min_samples_leaf=1, min_samples_split=5, n_estimators=100, max_depth=10))
+  ('scaler', StandardScaler()),
+  ('vc', VotingClassifier(estimators=[
+      ('rfc', RandomForestClassifier(random_state=42)),
+      ('svc', SVC(random_state=42, probability=True))
+  ], voting='soft')
+  )
 ])
 
-pipeline_B.fit(X_train, y_train)
+pipeline.fit(X_train, y_train)
 
 print("Saving models....")
-joblib.dump(pipeline_B, "Model_JEET.pkl", compress=3)
+joblib.dump(pipeline, "05_models/Model_JEET.pkl", compress=3)
+joblib.dump(pipeline,"06_deployment/model/JEET.pkl")
 print("Models saved successfully")
